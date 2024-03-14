9d4f0841245aa5b09abb181945b0ad37308ec903
devErLarsen <devErLarsen@protonmail.com>
2024-03-14 13:32:06 +0100

    oppgave 4

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 from geolocation import CityNotFoundException, get_lat_long
 from weather import print_weather
 
+valid_options = ['temp', 'regn', 'vind']
+
+def parse_input(input):
+    return [arg.strip() for arg in input.split(',')]
+
+def validate_options(options):
+    valid_arguments = [option for option in options if option in valid_options]
+    invalid_arguments = set(options) - set(valid_arguments)
+
+    if invalid_arguments or not options:
+        return False, f"Invalid argument(s) provided: {', '.join(invalid_arguments)}. enter only valid options!"
+    else:
+        return True, ''
+
 while True:
-    arg = input('Tast inn bynavn, eller 0 for å avslutte:\n')
+    arg = input('Tast inn bynavn, regn*, temp*, vind* eller 0 for å avslutte (* = optional):\n')
 
     if arg == '0':
         break
 
-    try:
-        lat, long = get_lat_long(city=arg)
-        print_weather(lat, long)
-    except CityNotFoundException as e:
-        print(e)
+    args = parse_input(arg)
+    city = args.pop(0)
+    valid, error_message = validate_options(args)
+
+    if valid:
+        try:
+            lat, long = get_lat_long(city)
+            print_weather(lat, long, args)
+        except CityNotFoundException as e:
+            print(e)
+    else:
+        print(error_message)
+
 
--- weather.py
+++ weather.py
@@ -1,8 +1,7 @@
 import requests
 from datetime import datetime, timedelta, timezone
 
-
-def print_weather(lat, lon):
+def print_weather(lat, lon, args):
     URL = "https://api.met.no/weatherapi/locationforecast/2.0/compact"
     PARAMS = { 'lat': lat, 'lon': lon }
     headers = {
@@ -12,7 +11,16 @@ def print_weather(lat, lon):
 
     data = request.json()
 
+    temp_option = 'temp' in args
+    wind_option = 'vind' in args
+    rain_option = 'regn' in args
+
     timeseries = data["properties"]["timeseries"]
+
+    units = data['properties']['meta']['units']
+    wind_speed_unit = units['wind_speed']
+    air_temperature_unit = units['air_temperature']
+    precipitation_amount_unit = units['precipitation_amount']
 
     periods = ['00-06', '06-12', '12-18', '18-00']
 
@@ -23,7 +31,7 @@ def print_weather(lat, lon):
         end_time = start_time + timedelta(days=1)
 
         all_day_temps = []
-        daily_temps = {period: [] for period in periods}
+        daily_temps = {period: {'temps': [], 'winds': [], 'rains': []} for period in periods}
         
 
         for entry in timeseries:
@@ -33,17 +41,43 @@ def print_weather(lat, lon):
                 period = get_period(hour)
                 temp = entry["data"]["instant"]["details"]["air_temperature"]
                 all_day_temps.append(temp)
-                #print(f"Kl {entry_time.strftime('%H:%M')} {temp} grader")
-                daily_temps[period].append(temp)
+                daily_temps[period]['temps'].append(temp)
+                daily_temps[period]['winds'].append(entry['data']['instant']['details']['wind_speed'])
 
 
-        print(f"---{start_time.strftime('%A %-d. %B %Y')} (snittemperatur {(sum(all_day_temps) / len(all_day_temps)):.1f} grader)---")
-        for period, temps in daily_temps.items():
-            if temps:
-                period_min_temp = min(temps)
-                period_max_temp = max(temps)
-                period_avg_temp = sum(temps) / len(temps)
-                print(f"{period}: fra {period_min_temp} til {period_max_temp} grader (snittemperatur {period_avg_temp:.1f} grader)")
+                if 'next_1_hours' in entry['data']:
+                    daily_temps[period]['rains'].append(entry['data']['next_1_hours']['details']['precipitation_amount'])
+                elif 'next_6_hours' in entry['data']:
+                    daily_temps[period]['rains'].append(entry['data']['next_6_hours']['details']['precipitation_amount'])
+                else:
+                    continue
+
+
+        if temp_option:
+            print(f"---{start_time.strftime('%A %-d. %B %Y')} (snittemperatur {(sum(all_day_temps) / len(all_day_temps)):.1f} grader {air_temperature_unit})---")
+        else:
+            print(f"---{start_time.strftime('%A %-d. %B %Y')}---")
+
+        for period in daily_temps:
+            print(f"{period}:")
+            temps = daily_temps[period]['temps']
+            winds = daily_temps[period]['winds']
+            rains = daily_temps[period]['rains']
+            if temp_option:
+                if temps:
+                    period_min_temp = min(temps)
+                    period_max_temp = max(temps)
+                    period_avg_temp = sum(temps) / len(temps)
+                    print(f"fra {period_min_temp} til {period_max_temp} grader {air_temperature_unit} (snittemperatur {period_avg_temp:.1f} grader {air_temperature_unit})")
+            if wind_option:
+                if winds:
+                    period_avg_wind = sum(winds) / len(winds)
+                    print(f"vind: {period_avg_wind:.1f} {wind_speed_unit} (avg)")
+
+            if rain_option:
+                if rains:
+                    sum_period_rain = sum(rains)
+                    print(f"regn: {sum_period_rain:.1f} {precipitation_amount_unit} (sum)")
 
 
 
@@ -56,7 +90,3 @@ def get_period(hour):
         return '12-18'
     else:
         return '18-00'
-
-
-
-
